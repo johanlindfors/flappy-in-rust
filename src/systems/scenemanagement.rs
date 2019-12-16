0f2c4f7c4812b534fcaf19b48a65d6cdccf771a0
Johan Lindfors <johan.lindfors@coderox.se>
2019-12-16 13:56:25 +0100

    Now using Scaler and showing the mouse

--- src/systems/scenemanagement.rs
+++ src/systems/scenemanagement.rs
@@ -1,6 +1,10 @@
 use tetra::window;
 use tetra::{Context, State};
+use tetra::graphics::scaling::{ScalingMode, ScreenScaler};
+use tetra::graphics::{self, Color};
+use tetra::math::Vec2;
 
+use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
 use crate::scenes::title::TitleScene;
 
 // === Scene Management ===
@@ -21,13 +25,21 @@ pub enum Transition {
 
 pub struct SceneManager {
     scenes: Vec<Box<dyn Scene>>,
+    scaler: ScreenScaler,
 }
 
 impl SceneManager {
     pub fn new(ctx: &mut Context) -> tetra::Result<SceneManager> {
+        window::set_mouse_visible(ctx)?;
         let initial_scene = TitleScene::new(ctx)?;
         Ok(SceneManager {
             scenes: vec![Box::new(initial_scene)],
+            scaler: ScreenScaler::with_window_size(
+                ctx,
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                ScalingMode::ShowAllPixelPerfect,
+            )?,
         })
     }
 }
@@ -51,10 +63,16 @@ impl State for SceneManager {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::set_canvas(ctx, self.scaler.canvas());
+
         match self.scenes.last_mut() {
             Some(active_scene) => active_scene.draw(ctx),
             None => window::quit(ctx),
         }
+
+        graphics::reset_canvas(ctx);
+        graphics::clear(ctx, Color::BLACK);
+        graphics::draw(ctx, &self.scaler, Vec2::new(0.0, 0.0));
 
         Ok(())
     }
