1a5969c636c82039b4e0e89872a3146d2d828197
Johan Lindfors <johan.lindfors@coderox.se>
2020-01-16 08:40:24 +0100

    Updated to latest crates, also fixed the scaler issue on MacBook Pro by removing Scaler

--- src/systems/scenemanagement.rs
+++ src/systems/scenemanagement.rs
@@ -1,30 +1,24 @@
-use tetra::graphics::scaling::{ScalingMode, ScreenScaler};
 use tetra::graphics::{self, Color};
-use tetra::math::Vec2;
 use tetra::window;
 use tetra::{Context, State};
 
 use crate::scenes::{title::TitleScene, Scene, Transition};
-use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
 pub struct SceneManager {
-    scenes: Vec<Box<dyn Scene>>,
-    scaler: ScreenScaler,
+    scenes: Vec<Box<dyn Scene>>
 }
 
 impl SceneManager {
     pub fn new(ctx: &mut Context) -> tetra::Result<SceneManager> {
-        window::set_mouse_visible(ctx)?;
-        let initial_scene = TitleScene::new(ctx)?;
-        Ok(SceneManager {
-            scenes: vec![Box::new(initial_scene)],
-            scaler: ScreenScaler::with_window_size(
-                ctx,
-                SCREEN_WIDTH,
-                SCREEN_HEIGHT,
-                ScalingMode::ShowAllPixelPerfect,
-            )?,
-        })
+        match window::set_mouse_visible(ctx, true) {
+            Ok(_) => {
+                let initial_scene = TitleScene::new(ctx)?;
+                Ok(SceneManager {
+                    scenes: vec![Box::new(initial_scene)]
+                })        
+            }
+            Err(e) => panic!("Couldn't show mouse: {:?}", e) 
+        }
     }
 }
 
@@ -47,16 +41,11 @@ impl State for SceneManager {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        graphics::set_canvas(ctx, self.scaler.canvas());
-
+        graphics::clear(ctx, Color::BLACK);
         match self.scenes.last_mut() {
             Some(active_scene) => active_scene.draw(ctx),
             None => window::quit(ctx),
         }
-
-        graphics::reset_canvas(ctx);
-        graphics::clear(ctx, Color::BLACK);
-        graphics::draw(ctx, &self.scaler, Vec2::new(0.0, 0.0));
 
         Ok(())
     }
